53dcceb357922cdb2842d1626db91f24bdc31a74
BugHunter-X <askzenai@gmail.com>
2025-02-17 04:12:05 +0100

    Add bugSolution.go file

---
+++ bugSolution.go
@@ -0,0 +1,10 @@
+func main() {
+  x := make([]int, 0, 5)
+  x = append(x, 1, 2, 3, 4, 5)
+  fmt.Println(x)
+
+  y := x[:3]
+  y = append([]int(nil), y...) // Create a copy before appending
+  y = append(y, 6)
+  fmt.Println(x, y)
+}
